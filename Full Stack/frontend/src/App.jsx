768fa88b36c99e8831529028256d9967f6ae6480
Dravani <42421220+Dravani@users.noreply.github.com>
2024-08-14 14:34:32 -0400

    update and delete working

--- Full Stack/frontend/src/App.jsx
+++ Full Stack/frontend/src/App.jsx
@@ -1,23 +1,56 @@
-import { useState, useEffect} from 'react'
-import ContactList from './ContactList'
-import './App.css'
+import { useState, useEffect } from "react";
+import ContactList from "./ContactList";
+import "./App.css";
+import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([])
+  const [contacts, setContacts] = useState([]);
+  const [ismodelOpen, setIsmodelOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
 
   useEffect(() => {
     fetchContacts()
-  }, [])
-
+  }, []);
 
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts")
-    const data = await response.json()
-    setContacts(data.contacts)
-    console.log(data.contacts)
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+  };
+
+  const closemodel = () => {
+    setIsmodelOpen(false)
+    setCurrentContact({})
   }
 
-  return <ContactList contacts={contacts}/>
+  const openCreatemodel = () => {
+    if (!ismodelOpen) setIsmodelOpen(true)
+  }
+
+  const openEditmodel = (contact) => {
+    if (ismodelOpen) return
+    setCurrentContact(contact)
+    setIsmodelOpen(true)
+  }
+
+  const onUpdate = () => {
+    closemodel()
+    fetchContacts()
+  }
+
+  return (
+    <>
+      <ContactList contacts={contacts} updateContact={openEditmodel} updateCallback={onUpdate} />
+      <button onClick={openCreatemodel}>Create New Contact</button>
+      {ismodelOpen && <div className="model">
+        <div className="model-content">
+          <span className="close" onClick={closemodel}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
+      </div>
+      }
+    </>
+  );
 }
 
-export default App
+export default App;
